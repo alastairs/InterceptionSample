c6871853094b72b68129dbcb8ae403ad965f414b
Alastair Smith <github@alastairsmith.me.uk>
2012-07-24 20:37:17 +0100

    Pass first set of product acceptance tests.
    A => 50, B => 30, C => 20, D => 15.

--- InterceptionSample.Tests/CheckoutFacts.cs
+++ InterceptionSample.Tests/CheckoutFacts.cs
@@ -16,6 +16,19 @@ namespace InterceptionSample.Tests
 
                 Assert.That(scannedItemPrice, Is.EqualTo(0));
             }
+
+            [TestCase("A", 50)]
+            [TestCase("B", 30)]
+            [TestCase("C", 20)]
+            [TestCase("D", 15)]
+            public void ReturnTheItemPriceWhenASingleItemIsScannedOnce(string sku, int price)
+            {
+                var checkout = new Checkout();
+
+                var scannedItemPrice = checkout.GetTotal(sku);
+
+                Assert.That(scannedItemPrice, Is.EqualTo(price));
+            }
         }
     }
 }
--- InterceptionSample/Checkout.cs
+++ InterceptionSample/Checkout.cs
@@ -1,9 +1,30 @@
+using System.Collections.Generic;
+
 namespace InterceptionSample
 {
     public class Checkout
     {
+        private readonly IDictionary<string, int> prices;
+
+        public Checkout()
+        {
+            prices = new Dictionary<string, int>
+            {
+                {"A", 50},
+                {"B", 30},
+                {"C", 20},
+                {"D", 15}
+            };
+        }
+
         public int GetTotal(string skus)
         {
+            int itemPrice;
+            if (prices.TryGetValue(skus, out itemPrice))
+            {
+                return itemPrice;
+            }
+
             return 0;
         }
     }
