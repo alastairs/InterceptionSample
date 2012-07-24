460f82fb9c80c22dc948356f6ac3792c934594c0
Alastair Smith <github@alastairsmith.me.uk>
2012-07-24 20:13:37 +0100

    First acceptance test.
    Scanning nothing should be a no-op.

---
+++ InterceptionSample.Tests/CheckoutFacts.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace InterceptionSample.Tests
+{
+    public class CheckoutFacts
+    {
+        [TestFixture, Category("Acceptance Tests")]
+        public class GetTotalShould
+        {
+            [Test]
+            public void Return0WhenNoItemIsScanned()
+            {
+                var checkout = new Checkout();
+
+                var scannedItemPrice = checkout.GetTotal(string.Empty);
+
+                Assert.That(scannedItemPrice, Is.EqualTo(0));
+            }
+        }
+    }
+}
---
+++ InterceptionSample/Checkout.cs
@@ -0,0 +1,10 @@
+namespace InterceptionSample
+{
+    public class Checkout
+    {
+        public int GetTotal(string skus)
+        {
+            return 0;
+        }
+    }
+}
